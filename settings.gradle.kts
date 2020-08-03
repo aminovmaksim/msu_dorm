4ff4c6c7e01ce8095c61c7e35b0fe48728bfc6d8
Max Aminov <aminov.md@skbkontur.ru>
2020-08-03 13:36:31 +0500

    Change to gradle

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "msu_dorm"
--- src/main/kotlin/com/devian/msu_dorm/controllers/MainController.kt
+++ src/main/kotlin/com/devian/msu_dorm/controllers/MainController.kt
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.core.io.ClassPathResource
 import org.springframework.core.io.Resource
 import org.springframework.scheduling.annotation.EnableScheduling
-import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.util.FileCopyUtils
